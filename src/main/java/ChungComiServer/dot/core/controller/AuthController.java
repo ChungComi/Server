5efcf5f8e16138dd3ab6bfa799491f3ce19b5d0d
jeongseonwoong <tjsdnd1219@naver.com>
2025-01-13 13:30:20 +0900

    add check method

--- src/main/java/ChungComiServer/dot/core/controller/AuthController.java
+++ src/main/java/ChungComiServer/dot/core/controller/AuthController.java
@@ -8,18 +8,17 @@ import ChungComiServer.dot.global.response.Response;
 import ChungComiServer.dot.global.security.util.JwtUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.InvalidPropertiesFormatException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Slf4j
 public class AuthController {
 
     private final AuthService authService;
@@ -42,6 +41,7 @@ public class AuthController {
     @PostMapping("/sign-up")
     public Response register(@Valid @RequestBody RegisterDTO registerDTO, BindingResult result){
         try{
+            log.info("register 컨트롤러 실행됨 id={},pw={}",registerDTO.getLoginId(),registerDTO.getLoginPw());
             if(result.hasErrors()){
                 StringBuilder stringBuilder = new StringBuilder();
                 result.getFieldErrors().forEach(fieldError -> stringBuilder.append(fieldError.getDefaultMessage()));
@@ -51,6 +51,11 @@ public class AuthController {
         } catch (InvalidPropertiesFormatException e) {
             return Response.failure(new ErrorCode(e.getMessage()));
         }
+    }
+
+    @GetMapping("")
+    public void check(){
+        log.info("왜 실행이 안되지?");
     }
 
 
