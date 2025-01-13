e91d841523c074f969f9868488a955ad49db9804
jeongseonwoong <tjsdnd1219@naver.com>
2025-01-13 13:28:27 +0900

    add custom filter between spring filters

--- src/main/java/ChungComiServer/dot/global/security/SecurityConfig.java
+++ src/main/java/ChungComiServer/dot/global/security/SecurityConfig.java
@@ -4,7 +4,12 @@ import ChungComiServer.dot.global.security.util.JwtUtil;
 import ChungComiServer.dot.global.security.util.PasswordUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -22,5 +27,17 @@ public class SecurityConfig {
     @Bean
     public JwtUtil jwtUtil(){
         return new JwtUtil();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable) // 비활성화 의도를 명시적으로 선언
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 상태 비저장
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil()), UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
+        return http.build();
     }
 }
