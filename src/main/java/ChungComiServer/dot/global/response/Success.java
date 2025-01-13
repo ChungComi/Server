81b469273842231dfbb9b9ed46c6aee8073eb16c
jeongseonwoong <tjsdnd1219@naver.com>
2025-01-13 13:44:25 +0900

    add @Getter

--- src/main/java/ChungComiServer/dot/global/response/Success.java
+++ src/main/java/ChungComiServer/dot/global/response/Success.java
@@ -1,8 +1,12 @@
 package ChungComiServer.dot.global.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
+@Getter
 public class Success<T> implements Result {
     private T data;
 }
