5441b398d42bfc891b6525da44a8337d4262fa04
jeongseonwoong <tjsdnd1219@naver.com>
2025-01-15 10:04:12 +0900

    add company layers

--- src/main/java/ChungComiServer/dot/core/controller/CompanyController.java
+++ src/main/java/ChungComiServer/dot/core/controller/CompanyController.java
@@ -1,14 +1,16 @@
 package ChungComiServer.dot.core.controller;
 
+import ChungComiServer.dot.core.dto.company.RegisterCompanyDTO;
 import ChungComiServer.dot.core.dto.company.ResponseCompanyDTO;
 import ChungComiServer.dot.core.service.CompanyService;
 import ChungComiServer.dot.global.response.ErrorCode;
 import ChungComiServer.dot.global.response.Response;
+import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +18,8 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/company")
-public class CompanyController {
+public class
+CompanyController {
 
     private final CompanyService companyService;
 
@@ -25,6 +28,38 @@ public class CompanyController {
         try{
             List<ResponseCompanyDTO> companyDTOs = companyService.findAll();
             return Response.success(companyDTOs);
+        }catch (Exception e){
+            return Response.failure(new ErrorCode(e.getMessage()));
+        }
+    }
+
+    @GetMapping("/{companyId}")
+    public Response getCompanyById(@PathVariable String companyId){
+        try{
+            ResponseCompanyDTO companyDTO = companyService.findById(companyId);
+            return Response.success(companyDTO);
+        }catch (Exception e){
+            return Response.failure(new ErrorCode(e.getMessage()));
+        }
+    }
+
+    @GetMapping("/{companyName}")
+    public Response getCompanyByName(@PathVariable String companyName){
+        try{
+            List<ResponseCompanyDTO> companyDTOs = companyService.findByName(companyName);
+            return Response.success(companyDTOs);
+        }catch (Exception e){
+            return Response.failure(new ErrorCode(e.getMessage()));
+        }
+    }
+
+    @PostMapping("/sign-up")
+    public Response registerCompany(@Valid @RequestBody RegisterCompanyDTO registerCompanyDTO, BindingResult result){
+        try {
+            if (result.hasErrors())
+                return Response.failure(new ErrorCode(result.getFieldError().toString()));
+            Long companyId = companyService.register(registerCompanyDTO.getName());
+            return Response.success(companyId);
         }catch (Exception e){
             return Response.failure(new ErrorCode(e.getMessage()));
         }
