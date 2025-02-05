5ec49203a16f7776e760a6054e6e3ca9ee5d7899
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-05 10:32:24 +0900

    시간표 응답 dto 생성

--- src/main/java/ChungComiServer/dot/core/dto/timetable/ResponseTimeTableDTO.java
+++ src/main/java/ChungComiServer/dot/core/dto/timetable/ResponseTimeTableDTO.java
@@ -1,2 +1,17 @@
-package ChungComiServer.dot.core.dto.timetable;public class ResponseTimeTableDTO {
+package ChungComiServer.dot.core.dto.timetable;
+
+import ChungComiServer.dot.core.enums.DayOfWeek;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ResponseTimeTableDTO {
+    private String className;
+    private String professor;
+    private DayOfWeek dayOfWeek;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
