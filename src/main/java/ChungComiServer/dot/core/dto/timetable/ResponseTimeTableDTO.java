d88d7a0ef3c1558b1131efe191390f9aa3b5cb29
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-05 13:36:55 +0900

    Entity를 DTO로 매핑하기 위한 생성자 추가

--- src/main/java/ChungComiServer/dot/core/dto/timetable/ResponseTimeTableDTO.java
+++ src/main/java/ChungComiServer/dot/core/dto/timetable/ResponseTimeTableDTO.java
@@ -1,10 +1,12 @@
 package ChungComiServer.dot.core.dto.timetable;
 
+import ChungComiServer.dot.core.entity.TimeTable;
 import ChungComiServer.dot.core.enums.DayOfWeek;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,4 +16,12 @@ public class ResponseTimeTableDTO {
     private DayOfWeek dayOfWeek;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+
+    public ResponseTimeTableDTO(TimeTable timeTable) {
+        this.className = timeTable.getClassName();
+        this.professor = timeTable.getClassName();
+        this.dayOfWeek = timeTable.getDayOfWeek();
+        this.startTime = timeTable.getStartTime();
+        this.endTime = timeTable.getEndTime();
+    }
 }
