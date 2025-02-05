1767306855f98448b3c3704937c3f2291fe47d9d
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-05 16:44:33 +0900

    일정을 위한 컨트롤러 서비스 레포지토리 계층 생성

---
+++ src/main/java/ChungComiServer/dot/core/repository/ScheduleRepository.java
@@ -0,0 +1,12 @@
+package ChungComiServer.dot.core.repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class ScheduleRepository {
+}
---
+++ src/main/java/ChungComiServer/dot/core/service/ScheduleService.java
@@ -0,0 +1,13 @@
+package ChungComiServer.dot.core.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ScheduleService {
+}
