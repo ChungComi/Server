466008beaee848aeea083d4daf33facb07735dd8
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-05 13:39:35 +0900

    로그인한 회원의 모든 시간표를 가져오는 서비스 계층 메서드 작성

--- src/main/java/ChungComiServer/dot/core/service/TimeTableService.java
+++ src/main/java/ChungComiServer/dot/core/service/TimeTableService.java
@@ -1,13 +1,26 @@
 package ChungComiServer.dot.core.service;
 
+import ChungComiServer.dot.core.dto.timetable.ResponseTimeTableDTO;
+import ChungComiServer.dot.core.entity.TimeTable;
+import ChungComiServer.dot.core.repository.TimeTableRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @Slf4j
 @RequiredArgsConstructor
 public class TimeTableService {
+
+    private final TimeTableRepository timeTableRepository;
+
+    public List<ResponseTimeTableDTO> findMyAllTimeTables(String stringUserId) {
+        Long userId = Long.valueOf(stringUserId);
+        List<TimeTable> timeTables =  timeTableRepository.findMyAllTimeTables(userId);
+        return timeTables.stream().map(ResponseTimeTableDTO::new).toList();
+    }
 }
