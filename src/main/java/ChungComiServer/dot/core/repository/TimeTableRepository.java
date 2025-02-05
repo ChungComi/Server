c61cc237f3a632ab992831ab2b6ed570da0b05fc
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-05 13:38:56 +0900

    로그인한 회원의id를 통해 모든 시간표를 가져오는jpql문 작성

--- src/main/java/ChungComiServer/dot/core/repository/TimeTableRepository.java
+++ src/main/java/ChungComiServer/dot/core/repository/TimeTableRepository.java
@@ -1,11 +1,23 @@
 package ChungComiServer.dot.core.repository;
 
+import ChungComiServer.dot.core.entity.TimeTable;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 @Slf4j
 public class TimeTableRepository {
+
+    private final EntityManager em;
+
+    public List<TimeTable> findMyAllTimeTables(Long userId) {
+        return em.createQuery("select t from TimeTable t join fetch t.member where t.member.id =:userId", TimeTable.class)
+                .setParameter("userId",userId)
+                .getResultList();
+    }
 }
