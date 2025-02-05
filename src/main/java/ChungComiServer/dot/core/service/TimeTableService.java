0a6a5e409e52d6906eb8f41809c8600cc3a70dc9
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-05 10:10:09 +0900

    시간표를 위한 서비스 계층 생성

---
+++ src/main/java/ChungComiServer/dot/core/service/TimeTableService.java
@@ -0,0 +1,13 @@
+package ChungComiServer.dot.core.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@Slf4j
+@RequiredArgsConstructor
+public class TimeTableService {
+}
