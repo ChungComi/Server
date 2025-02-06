c354d1c6fc61f41a2521653f91c6ccf50d26aa98
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-06 10:25:27 +0900

    일정 추가를 위한 dto 만들기

--- src/main/java/ChungComiServer/dot/core/dto/schedule/RegisterScheduleDTO.java
+++ src/main/java/ChungComiServer/dot/core/dto/schedule/RegisterScheduleDTO.java
@@ -1,2 +1,16 @@
-package ChungComiServer.dot.core.dto.schedule;public class RegisterScheduleDTO {
+package ChungComiServer.dot.core.dto.schedule;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class RegisterScheduleDTO {
+    @NotEmpty
+    private String content;
+    @NotEmpty
+    private LocalDateTime date;
 }
