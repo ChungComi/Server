e336d844ccbfa4b878cc736d8eb30988406b6fa0
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-13 11:11:21 +0900

    @Getter 추가

---
+++ src/main/java/ChungComiServer/dot/api/dto/ResponsePostDTOForBoard.java
@@ -0,0 +1,8 @@
+package ChungComiServer.dot.api.dto;
+
+public class ResponsePostDTO_T_A_V_D {
+
+    private String title;
+    private Long likes;
+    private Long views;
+}
--- src/main/java/ChungComiServer/dot/global/BaseEntity.java
+++ src/main/java/ChungComiServer/dot/global/BaseEntity.java
@@ -1,10 +1,12 @@
 package ChungComiServer.dot.global;
 
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@Getter
 public abstract class BaseEntity {
 
     protected LocalDateTime registerDate;
